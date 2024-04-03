ce1f8fee60bfea8d3568447864b854aba0331e0f
kassandra falsitta <160047671+kassandra2000@users.noreply.github.com>
2024-04-03 18:07:01 +0200

    vv

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const NascondiSezione=document.querySelectorAll("")
+
+const nascondiSezione = document.querySelectorAll(".nascSez");
+console.log(nascondiSezione)
+for (let i = 0; i < nascondiSezione.length; i++) {
+   
+    
+
+nascondiSezione[i].onclick = function () {
+  const divNascondiSezione=this.parentNode.parentElement.childNodes[3]
+//   divNascondiSezione.className.toggle("collapse")
+divNascondiSezione.classList.toggle("collapse")
+
+
+};
+}
